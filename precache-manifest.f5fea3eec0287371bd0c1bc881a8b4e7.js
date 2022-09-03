ad8c1c9a19d3c47f6170bd6159d61797ada6ae49
saeed hussain <80773074+saeedbarcha@users.noreply.github.com>
2022-09-03 13:14:25 -0700

    files inside build folder
    files

---
+++ precache-manifest.f5fea3eec0287371bd0c1bc881a8b4e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1582a12857d2b60ca218",
+    "url": "/static/css/main.6bd0f4f2.chunk.css"
+  },
+  {
+    "revision": "1582a12857d2b60ca218",
+    "url": "/static/js/main.1582a128.chunk.js"
+  },
+  {
+    "revision": "2904430ec9b47d0c8f70",
+    "url": "/static/css/1.2fa526dd.chunk.css"
+  },
+  {
+    "revision": "2904430ec9b47d0c8f70",
+    "url": "/static/js/1.2904430e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23357838c3841cf95bedbdac49f663c0",
+    "url": "/static/media/h-bg1.23357838.jpg"
+  },
+  {
+    "revision": "01b7cba39b5035d3472990bc87096111",
+    "url": "/static/media/h-bg2.01b7cba3.jpg"
+  },
+  {
+    "revision": "987c5b9481203bc6857c26eb7d22852f",
+    "url": "/static/media/h-bg3.987c5b94.jpg"
+  },
+  {
+    "revision": "339a789808038a83ca2fad2fcd85b3e5",
+    "url": "/static/media/a.339a7898.jpg"
+  },
+  {
+    "revision": "db8101346a90e972d6990de4cf0efb6d",
+    "url": "/static/media/b.db810134.jpg"
+  },
+  {
+    "revision": "6ba749e4a8cdc76aaa51a67940e96079",
+    "url": "/static/media/c.6ba749e4.jpg"
+  },
+  {
+    "revision": "bc310f596ce4bdb305ba5c0caf2287c8",
+    "url": "/index.html"
+  }
+];
